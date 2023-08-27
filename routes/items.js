fe0202e8397b45cd9f8c498e94830f382fcfcd2f
Sooraj B <sooraj.b@webandcrafts.com>
2023-08-27 21:19:58 +0530

    item endpoint changed

--- routes/items.js
+++ routes/items.js
@@ -4,7 +4,7 @@ import isAuthorized from "../middlewares/authorization.js";
 
 const router = express.Router();
 
-router.post("/add", isAuthorized, addItem);
+router.post("/create", isAuthorized, addItem);
 router.get("/list", isAuthorized, listItems);
 router.delete("/delete/:id", isAuthorized, deleteItem);
 
